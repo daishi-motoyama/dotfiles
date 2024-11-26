f11b00069b27c4d46664d2fd1f6aa3a528a9c90e
daishi-motoyama <48724872dad@gmail.com>
2024-11-26 11:15:46 +0900

    ♻️ refactor: シェルスクリプトのリファクタリング

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 下記のリポジトリを参考に実装
-# https://github.com/nicknisi/dotfiles/blob/699b1327404b826df285e997e14ddac3a08cb7a2/install.sh 
+# https://github.com/nicknisi/dotfiles/blob/699b1327404b826df285e997e14ddac3a08cb7a2/install.sh
 
 DOTFILES="$(pwd)"
 COLOR_GRAY="\033[1;38;5;243m"
@@ -44,17 +44,6 @@ backup() {
   echo "Creating backup directory at $BACKUP_DIR"
   mkdir -p "$BACKUP_DIR"
 
-  for file in "${linkables[@]}"; do
-    filename="$(basename "$file")"
-    target="$HOME/$filename"
-    if [ -f "$target" ]; then
-      echo "backing up $filename"
-      cp "$target" "$BACKUP_DIR"
-    else
-      warning "$filename does not exist at this location or is a symlink"
-    fi
-  done
-
   for filename in "$HOME/.config/nvim" "$HOME/.vim" "$HOME/.vimrc"; do
     if [ ! -L "$filename" ]; then
       echo "backing up $filename"
@@ -67,17 +56,6 @@ backup() {
 
 cleanup_symlinks() {
   title "Cleaning up symlinks"
-  for file in "${linkables[@]}"; do
-    target="$HOME/$(basename "$file")"
-    if [ -L "$target" ]; then
-      info "Cleaning up \"$target\""
-      rm "$target"
-    elif [ -e "$target" ]; then
-      warning "Skipping \"$target\" because it is not a symlink"
-    else
-      warning "Skipping \"$target\" because it does not exist"
-    fi
-  done
 
   echo -e
   info "installing to $config_home"
@@ -98,16 +76,6 @@ cleanup_symlinks() {
 
 setup_symlinks() {
   title "Creating symlinks"
-
-  for file in "${linkables[@]}"; do
-    target="$HOME/$(basename "$file")"
-    if [ -e "$target" ]; then
-      info "~${target#"$HOME"} already exists... Skipping."
-    else
-      info "Creating symlink for $file"
-      ln -s "$DOTFILES/$file" "$target"
-    fi
-  done
 
   echo -e
   info "installing to $config_home"
@@ -137,7 +105,7 @@ setup_symlinks() {
     info "Creating symlink for .zshenv"
     ln -s "$DOTFILES/config/zsh/.zshenv" "$HOME/.zshenv"
   else
-    info "~/.zshenv already exists... Skipping."
+    info "$HOME/.zshenv already exists... Skipping."
   fi
 }
 
@@ -149,7 +117,7 @@ setup_docker_compose() {
     info "Docker compose is already installed, so skip it."
   else
     title "Installing Docker Compose"
-    mkdir -p "$DOCKER_CLI_PLUGINS_DIR"   
+    mkdir -p "$DOCKER_CLI_PLUGINS_DIR"
     curl -SL "https://github.com/docker/compose/releases/download/v2.30.3/docker-compose-darwin-aarch64" -o "$DOCKER_CLI_PLUGINS_DIR/docker-compose"
     chmod +x "$DOCKER_CLI_PLUGINS_DIR/docker-compose"
     info "Installed docker compose"
@@ -167,7 +135,7 @@ setup_homebrew() {
 
   # install brew dependencies from Brewfile
   brew bundle --file="~/.config/homebrew/Brewfile"
-  
+
   info "Homebrew setup complete"
 }
 
@@ -196,31 +164,31 @@ replace_zsh() {
 }
 
 case "$1" in
-backup)
-  backup
-  ;;
-clean)
-  cleanup_symlinks
-  ;;
-link)
-  setup_symlinks
-  ;;
-docker)
-  setup_docker_compose
-  ;;
-homebrew)
-  setup_homebrew
-  ;;
-all)
-  setup_symlinks
-  setup_homebrew
-  replace_zsh
-  setup_docker_compose
-  ;;
-*)
-  echo -e $"\nUsage: $(basename "$0") {backup|link|git|homebrew|shell|terminfo|macos|all}\n"
-  exit 1
-  ;;
+  backup)
+    backup
+    ;;
+  clean)
+    cleanup_symlinks
+    ;;
+  link)
+    setup_symlinks
+    ;;
+  docker)
+    setup_docker_compose
+    ;;
+  homebrew)
+    setup_homebrew
+    ;;
+  all)
+    setup_symlinks
+    setup_homebrew
+    replace_zsh
+    setup_docker_compose
+    ;;
+  *)
+    echo -e $"\nUsage: $(basename "$0") {backup|link|git|homebrew|shell|terminfo|macos|all}\n"
+    exit 1
+    ;;
 esac
 
 echo -e
