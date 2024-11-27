e47d7211c068375ce66c1b6e3544be380900efd6
daishi-motoyama <48724872dad@gmail.com>
2024-11-27 16:23:36 +0900

    ➕ add: Hop.nvimでファイル内の移動を高速化する

---
+++ config/nvim/lua/daishi/plugins/hop-nvim.lua
@@ -0,0 +1,28 @@
+return {
+  "smoka7/hop.nvim",
+  version = "*",
+  config = function()
+    local hop = require("hop")
+    local directions = require("hop.hint").HintDirection
+    local keymap = vim.keymap
+
+    keymap.set({ "n", "v", "o" }, "f", "<cmd>HopChar1CurrentLineAC<CR>")
+    keymap.set({ "n", "v", "o" }, "F", "<cmd>HopChar1CurrentLineBC<CR>")
+    keymap.set({ "n", "v", "o" }, "t", function()
+      hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+    end, { remap = true })
+    keymap.set({ "n", "v", "o" }, "T", function()
+      hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+    end, { remap = true })
+    keymap.set("n", "<leader>hw", "<cmd>HopWord<CR>", { desc = "Hop Word" })
+    keymap.set("n", "<leader>hp", "<cmd>HopPattern<CR>", { desc = "Hop Pattern" })
+    keymap.set("n", "<leader>hv", "<cmd>HopVertical<CR>", { desc = "Hop Vertical" })
+    keymap.set("n", "<leader>ha", "<cmd>HopAnywhere<CR>", { desc = "Hop Anywhere" })
+
+    hop.setup({
+      jump_on_sole_occurrence = false,
+      case_insensitive = false,
+      multi_windows = true,
+    })
+  end,
+}
