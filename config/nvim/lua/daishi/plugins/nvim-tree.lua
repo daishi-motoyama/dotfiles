d50fbabde8e6fc6129f94e9b2426944cd8d64ec3
daishi-motoyama <48724872dad@gmail.com>
2024-11-28 18:42:06 +0900

    :wrench: floating windowに変更

--- config/nvim/lua/daishi/plugins/nvim-tree.lua
+++ config/nvim/lua/daishi/plugins/nvim-tree.lua
@@ -3,14 +3,38 @@ return {
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
     local nvimtree = require("nvim-tree")
+    local HEIGHT_RATIO = 0.8
+    local WIDTH_RATIO = 0.8
 
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
-    
+
     nvimtree.setup({
       view = {
-        width = 35,
-        relativenumber = true,
+        float = {
+          enable = true,
+          open_win_config = function()
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+            local window_w = screen_w * WIDTH_RATIO
+            local window_h = screen_h * HEIGHT_RATIO
+            local window_w_int = math.floor(window_w)
+            local window_h_int = math.floor(window_h)
+            local center_x = (screen_w - window_w) / 2
+            local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+            return {
+              border = "rounded",
+              relative = "editor",
+              row = center_y,
+              col = center_x,
+              width = window_w_int,
+              height = window_h_int,
+            }
+          end,
+        },
+        width = function()
+          return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+        end,
       },
       renderer = {
         indent_markers = {
@@ -45,5 +69,5 @@ return {
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
     keymap.set("n", "<leader>eo", "<cmd>NvimTreeFocus<CR>", { desc = "Focus file explorer" })
-  end
+  end,
 }
