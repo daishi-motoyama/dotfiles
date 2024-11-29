d202fb751a88f5d2c3e10495d4e5fe830b699106
daishi-motoyama <48724872dad@gmail.com>
2024-11-29 10:00:24 +0900

    🔧 config: telescope の frecency 拡張機能を追加

--- config/nvim/lua/daishi/plugins/telescope.lua
+++ config/nvim/lua/daishi/plugins/telescope.lua
@@ -23,6 +23,11 @@ return {
           },
         },
       },
+      extensions = {
+        frecency = {
+          show_scores = true,
+        },
+      },
     })
 
     telescope.load_extension("fzf")
@@ -31,11 +36,14 @@ return {
     local keymap = vim.keymap
     -- keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Find files in cwd" })
     keymap.set("n", "<leader>ff", "<cmd>Telescope git_files<cr>", { desc = "Find files in git repository" })
-    -- keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
     keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<cr>", { desc = "Find word" })
     keymap.set("n", "<leader>fg", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope frecency<cr>", { desc = "Find recent files" })
+    keymap.set("n", "<leader>fr", function()
+      require("telescope").extensions.frecency.frecency({
+        workspace = "CWD",
+      })
+    end, { desc = "Find recent files" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope command_history<cr>", { desc = "Find command history" })
     keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Find help tags" })
     keymap.set("n", "<leader>fC", "<cmd>Telescope colorscheme<cr>", { desc = "Find colorscheme" })
