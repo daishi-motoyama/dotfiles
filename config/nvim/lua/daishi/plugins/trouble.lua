a779b95d383f0d7221ce2288fb5cbc0917eee6e8
daishi-motoyama <48724872dad@gmail.com>
2024-11-29 18:34:38 +0900

    ♻️ refactor: Trouble.nvimのキーマップ設定を改善

--- config/nvim/lua/daishi/plugins/trouble.lua
+++ config/nvim/lua/daishi/plugins/trouble.lua
@@ -1,45 +1,34 @@
 return {
   "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "folke/todo-comments.nvim",
+  },
   cmd = "Trouble",
-  opts = {
-    focus = true,
-  },
-  keys = {
-    {
+  config = function()
+    local trouble = require("trouble")
+    local keymap = vim.keymap
+
+    trouble.setup({
+      focus = true,
+    })
+
+    keymap.set("n", "<leader>xt", "<cmd>Trouble diagnostics toggle<CR>", { desc = "Diagnostics (Trouble)" })
+    keymap.set(
+      "n",
       "<leader>xx",
-      "<cmd>Trouble diagnostics toggle<CR>",
-      desc = "Diagnostics (Trouble)",
-    },
-    {
-      "<leader>xX",
       "<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
-      desc = "Buffer Diagnostics (Trouble)",
-    },
-    {
-      "<leader>xs",
-      "<cmd>Trouble symbols toggle focus=false<CR>",
-      desc = "Symbols (Trouble)",
-    },
-    {
+      { desc = "Buffer Diagnostics (Trouble)" }
+    )
+    keymap.set("n", "<leader>xs", "<cmd>Trouble symbols toggle<CR>", { desc = "Symbols (Trouble)" })
+    keymap.set(
+      "n",
       "<leader>xl",
-      "<cmd>Trouble lsp toggle focus=false win.position=right<CR>",
-      desc = "LSP Definitions / references / ... (Trouble)",
-    },
-    {
-      "<leader>xL",
-      "<cmd>Trouble loclist toggle<CR>",
-      desc = "Location List (Trouble)",
-    },
-    {
-      "<leader>xq",
-      "<cmd>Trouble qflist toggle<CR>",
-      desc = "Quickfix List (Trouble)",
-    },
-    {
-      "<leader>xt",
-      "<cmd>TodoTrouble<CR>",
-      desc = "Open todos in trouble",
-    },
-  },
+      "<cmd>Trouble lsp toggle win.position=right<CR>",
+      { desc = "LSP Definitions / references / ... (Trouble)" }
+    )
+    keymap.set("n", "<leader>xL", "<cmd>Trouble loclist toggle<CR>", { desc = "Location list (Trouble)" })
+    keymap.set("n", "<leader>xq", "<cmd>Trouble qflist toggle<CR>", { desc = "Quickfix list (Trouble)" })
+    keymap.set("n", "<leader>xT", "<cmd>TodoTrouble<CR>", { desc = "Open todos in trouble" })
+  end,
 }
