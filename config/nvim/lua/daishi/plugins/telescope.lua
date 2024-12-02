9deed87b80942dd966785ba42539412c3358c93c
daishi-motoyama <48724872dad@gmail.com>
2024-12-02 17:24:59 +0900

    :sparkles:yanky.nvimでヤンク履歴管理を実装

--- config/nvim/lua/daishi/plugins/telescope.lua
+++ config/nvim/lua/daishi/plugins/telescope.lua
@@ -93,23 +93,21 @@ return {
     telescope.load_extension("fzf")
     telescope.load_extension("frecency")
     telescope.load_extension("file_browser")
+    telescope.load_extension("yank_history")
 
     local keymap = vim.keymap
-    keymap.set("n", "<leader>ff", "<cmd>Telescope git_files<cr>", { desc = "Find files in git repository" })
-    keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<cr>", { desc = "Find word" })
-    keymap.set("n", "<leader>fg", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
-    keymap.set("n", "<leader>fr", function()
-      require("telescope").extensions.frecency.frecency({
-        workspace = "CWD",
-      })
-    end, { desc = "Find recent files" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope command_history<cr>", { desc = "Find command history" })
-    keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { desc = "Find help tags" })
-    keymap.set("n", "<leader>fC", "<cmd>Telescope colorscheme<cr>", { desc = "Find colorscheme" })
-    keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Find keymaps" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope search_history<cr>", { desc = "Find search history" })
-    keymap.set("n", "<leader>fb", ":Telescope file_browser<CR>", { desc = "Open file browser" })
-    keymap.set("n", "<leader>fR", ":Telescope registers<CR>", { desc = "Find registersOpen file browser" })
+    keymap.set("n", "<leader>ff", "<cmd>Telescope git_files<CR>", { desc = "Find files in git repository" })
+    keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "Find word" })
+    keymap.set("n", "<leader>fg", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find todos" })
+    keymap.set("n", "<leader>fr", "<cmd>Telescope frecency workspace=CWD<CR>", { desc = "Find recent files" })
+    keymap.set("n", "<leader>fc", "<cmd>Telescope command_history<CR>", { desc = "Find command history" })
+    keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Find help tags" })
+    keymap.set("n", "<leader>fC", "<cmd>Telescope colorscheme<CR>", { desc = "Find colorscheme" })
+    keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<CR>", { desc = "Find keymaps" })
+    keymap.set("n", "<leader>fs", "<cmd>Telescope search_history<CR>", { desc = "Find search history" })
+    keymap.set("n", "<leader>fb", "<cmd>Telescope file_browser<CR>", { desc = "Open file browser" })
+    keymap.set("n", "<leader>fR", "<cmd>Telescope registers<CR>", { desc = "Find registersOpen file browser" })
+    keymap.set("n", "<leader>fy", "<cmd>Telescope yank_history<CR>", { desc = "Find yank history" })
   end,
 }
