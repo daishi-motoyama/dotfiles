b4ff7b4ff9ed96d3d79568f8f24e0fa5db20b339
daishi-motoyama <48724872dad@gmail.com>
2024-12-03 13:30:08 +0900

    :lipstick:カラーテーマをGitHubに変更

--- config/nvim/lua/daishi/plugins/colorscheme.lua
+++ config/nvim/lua/daishi/plugins/colorscheme.lua
@@ -1,12 +1,26 @@
+-- NOTE: everforest
+-- return {
+--   "neanias/everforest-nvim",
+--   priority = 1000,
+--   config = function()
+--     local everforest = require("everforest")
+--     everforest.setup({
+--       background = "soft",
+--       transparent_background_level = 0,
+--     })
+--     vim.cmd("colorscheme everforest")
+--   end,
+-- }
+
+-- NOTE: github-theme
 return {
-  "neanias/everforest-nvim",
+  "projekt0n/github-nvim-theme",
+  name = "github-theme",
+  lazy = false,
   priority = 1000,
   config = function()
-    local everforest = require("everforest")
-    everforest.setup({
-      background = "soft",
-      transparent_background_level = 0,
-    })
-    vim.cmd("colorscheme everforest")
+    require("github-theme").setup()
+
+    vim.cmd("colorscheme github_light")
   end,
 }
--- config/nvim/lua/daishi/plugins/lualine.lua
+++ config/nvim/lua/daishi/plugins/lualine.lua
@@ -7,14 +7,13 @@ return {
 
     lualine.setup({
       optoins = {
-        theme = "everforest",
+        theme = "github",
       },
       sections = {
         lualine_x = {
           {
             lazy_status.updates,
             cond = lazy_status.has_updates,
-            color = { fg = "#ff9e64" },
           },
           { "encoding" },
           { "fileformat" },
