e73054e4d5ab3aec353432020aa2bf9d762d9392
daishi-motoyama <48724872dad@gmail.com>
2024-12-04 16:48:46 +0900

    🔧 config: TreeSJの遅延ローディングを追加

--- config/nvim/lua/daishi/plugins/treesj.lua
+++ config/nvim/lua/daishi/plugins/treesj.lua
@@ -1,5 +1,6 @@
 return {
   "Wansmer/treesj",
+  event = { "BufReadPre", "BufNewFile" },
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   config = function()
     local treesj = require("treesj")
