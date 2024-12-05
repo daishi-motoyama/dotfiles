654abce3998669a18231e339fa10e3396520eb8c
daishi-motoyama <48724872dad@gmail.com>
2024-12-05 11:30:32 +0900

    🎨 style: エイリアス設定を簡素化

--- config/zsh/alias.zsh
+++ config/zsh/alias.zsh
@@ -3,17 +3,17 @@
 # https://gist.github.com/AppleBoiy/04a249b6f64fd0fe1744aff759a0563b
 abbr -g ll='eza -la --icons --octal-permissions --group-directories-first'
 abbr -g l='eza -bGF --header --git --color=always --group-directories-first --icons'
-abbr -g llm='eza -lbGd --header --git --sort=modified --color=always --group-directories-first --icons' 
+abbr -g llm='eza -lbGd --header --git --sort=modified --color=always --group-directories-first --icons'
 abbr -g la='eza --long --all --group --group-directories-first'
 abbr -g lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --color=always --group-directories-first --icons'
 abbr -g lS='eza -1 --color=always --group-directories-first --icons'
 abbr -g lt='eza --tree --level=2 --color=always --group-directories-first --icons'
 
 #upstairs
-abbr -g ...='cd ../..'
-abbr -g ....='cd ../../..'
-abbr -g .....='cd ../../../..'
-abbr -g ......='cd ../../../../..'
+abbr -g ...='../..'
+abbr -g ....='../../..'
+abbr -g .....='../../../..'
+abbr -g ......='../../../../..'
 
 # git
 abbr -g gl="git log --graph --all --pretty=format:'%C(yellow)%h%C(cyan)%d%Creset %s %C(white)- %an, %ar%Creset'"
@@ -31,4 +31,3 @@ abbr -g Lg="lazygit"
 
 # lazydocker
 abbr -g Ld="lazydocker"
-
