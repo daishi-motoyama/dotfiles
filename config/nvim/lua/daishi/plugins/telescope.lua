ceebfaac87c0e8cc24d1c20f300d7bf404de1ede
daishi-motoyama <48724872dad@gmail.com>
2024-12-13 10:59:11 +0900

    ➖ remove: Telescope File Browser を削除

--- config/nvim/lua/daishi/plugins/telescope.lua
+++ config/nvim/lua/daishi/plugins/telescope.lua
@@ -7,7 +7,6 @@ return {
     "nvim-tree/nvim-web-devicons",
     "folke/todo-comments.nvim",
     "nvim-telescope/telescope-frecency.nvim",
-    "nvim-telescope/telescope-file-browser.nvim",
   },
   config = function()
     local telescope = require("telescope")
@@ -44,43 +43,6 @@ return {
         frecency = {
           show_scores = true,
         },
-        file_browser = {
-          hijack_netrw = true,
-          mappings = {
-            ["i"] = {
-              ["<C-a>"] = fb_actions.create,
-              ["<S-CR>"] = false,
-              ["<C-r>"] = fb_actions.rename,
-              ["<C-m>"] = fb_actions.move,
-              ["<C-y>"] = fb_actions.copy,
-              ["<C-d>"] = fb_actions.remove,
-              ["<C-o>"] = false,
-              ["<C-g>"] = fb_actions.goto_parent_dir,
-              ["<C-e>"] = false,
-              ["<C-w>"] = fb_actions.goto_cwd,
-              ["<C-t>"] = fb_actions.change_cwd,
-              ["<C-f>"] = fb_actions.toggle_browser,
-              ["<C-h>"] = fb_actions.toggle_hidden,
-              ["<C-s>"] = fb_actions.toggle_all,
-              ["<bs>"] = fb_actions.backspace,
-            },
-            ["n"] = {
-              ["a"] = fb_actions.create,
-              ["r"] = fb_actions.rename,
-              ["m"] = fb_actions.move,
-              ["y"] = fb_actions.copy,
-              ["d"] = fb_actions.remove,
-              ["o"] = false,
-              ["g"] = fb_actions.goto_parent_dir,
-              ["e"] = false,
-              ["w"] = fb_actions.goto_cwd,
-              ["t"] = fb_actions.change_cwd,
-              ["f"] = fb_actions.toggle_browser,
-              ["h"] = fb_actions.toggle_hidden,
-              ["s"] = fb_actions.toggle_all,
-            },
-          },
-        },
         fzf = {
           fuzzy = true,
           override_generic_sorter = true,
@@ -92,7 +54,6 @@ return {
 
     telescope.load_extension("fzf")
     telescope.load_extension("frecency")
-    telescope.load_extension("file_browser")
     telescope.load_extension("yank_history")
 
     local keymap = vim.keymap
@@ -106,7 +67,6 @@ return {
     keymap.set("n", "<leader>fC", "<cmd>Telescope colorscheme<CR>", { desc = "Find colorscheme" })
     keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<CR>", { desc = "Find keymaps" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope search_history<CR>", { desc = "Find search history" })
-    keymap.set("n", "<leader>fb", "<cmd>Telescope file_browser<CR>", { desc = "Open file browser" })
     keymap.set("n", "<leader>fR", "<cmd>Telescope registers<CR>", { desc = "Find registersOpen file browser" })
     keymap.set("n", "<leader>fy", "<cmd>Telescope yank_history<CR>", { desc = "Find yank history" })
   end,
