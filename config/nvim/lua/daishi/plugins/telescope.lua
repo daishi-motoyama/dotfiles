9ce13f5859c36cc8aaba282f39a780e8cf6c1bda
daishi-motoyama <48724872dad@gmail.com>
2024-12-13 16:47:13 +0900

    ♻️ refactor:telescopeの検索機能を改善

--- config/nvim/lua/daishi/plugins/telescope.lua
+++ config/nvim/lua/daishi/plugins/telescope.lua
@@ -11,6 +11,7 @@ return {
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
+    local builtin = require("telescope.builtin")
 
     local toggle_modes = function()
       local mode = vim.api.nvim_get_mode().mode
@@ -55,15 +56,61 @@ return {
     telescope.load_extension("frecency")
     telescope.load_extension("yank_history")
 
+    local function is_git_repo()
+      vim.fn.system("git rev-parse --is-inside-work-tree")
+
+      return vim.v.shell_error == 0
+    end
+
+    local function get_git_root()
+      local dot_git_path = vim.fn.finddir(".git", ".;")
+      return vim.fn.fnamemodify(dot_git_path, ":h")
+    end
+
+    function live_grep_from_project_git_root()
+      local opts = {}
+
+      if is_git_repo() then
+        opts = {
+          cwd = get_git_root(),
+          path_display = { "absolute" },
+        }
+      end
+
+      builtin.live_grep(opts)
+    end
+
+    function grep_string_from_project_git_root()
+      local opts = {}
+      if is_git_repo() then
+        opts = {
+          cwd = get_git_root(),
+          path_display = { "absolute" },
+        }
+      end
+      builtin.grep_string(opts)
+    end
+
     local keymap = vim.keymap
     keymap.set("n", "<leader>ff", "<cmd>Telescope git_files<CR>", { desc = "Find files in git repository" })
-    keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<CR>", { desc = "Find word" })
-    keymap.set("n", "<leader>fg", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>fwc", "<cmd>Telescope live_grep<CR>", { desc = "Find word in cwd" })
+    keymap.set(
+      "n",
+      "<leader>fwr",
+      "<cmd>lua live_grep_from_project_git_root()<CR>",
+      { desc = "Find word in project root" }
+    )
+    keymap.set("n", "<leader>fgc", "<cmd>Telescope grep_string<CR>", { desc = "Find string under cursor in cwd" })
+    keymap.set(
+      "n",
+      "<leader>fgr",
+      "<cmd>lua grep_string_from_project_git_root()<CR>",
+      { desc = "Find string under cursor in project root" }
+    )
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<CR>", { desc = "Find todos" })
     keymap.set("n", "<leader>fr", "<cmd>Telescope frecency workspace=CWD<CR>", { desc = "Find recent files" })
     keymap.set("n", "<leader>fc", "<cmd>Telescope command_history<CR>", { desc = "Find command history" })
     keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", { desc = "Find help tags" })
-    keymap.set("n", "<leader>fC", "<cmd>Telescope colorscheme<CR>", { desc = "Find colorscheme" })
     keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<CR>", { desc = "Find keymaps" })
     keymap.set("n", "<leader>fs", "<cmd>Telescope search_history<CR>", { desc = "Find search history" })
     keymap.set("n", "<leader>fR", "<cmd>Telescope registers<CR>", { desc = "Find registersOpen file browser" })
