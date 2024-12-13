72a88c1e0a06708dd99a70fa30633b8a6d717a47
daishi-motoyama <48724872dad@gmail.com>
2024-12-13 16:48:14 +0900

    ➖ remove: プロジェクト管理プラグインを削除

--- config/nvim/lua/daishi/plugins/project.lua
+++
@@ -1,9 +0,0 @@
-return {
-	"ahmedkhalf/project.nvim",
-	dependencies = { "nvim-telescope/telescope.nvim" },
-	event = "VeryLazy",
-	config = function()
-		require("telescope").load_extension("projects")
-		require("project_nvim").setup()
-	end,
-}
