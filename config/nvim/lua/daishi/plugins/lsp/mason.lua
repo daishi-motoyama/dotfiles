32b604e4f2e32af88807301d093ef060534e9096
daishi-motoyama <48724872dad@gmail.com>
2024-12-14 17:12:26 +0900

    ➕ add: mason-null-ls プラグインの自動設定を追加

--- config/nvim/lua/daishi/plugins/lsp/mason.lua
+++ config/nvim/lua/daishi/plugins/lsp/mason.lua
@@ -3,11 +3,17 @@ return {
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
+    "jay-babu/mason-null-ls.nvim",
   },
   config = function()
     local mason = require("mason")
     local mason_lspconfig = require("mason-lspconfig")
     local mason_tool_installer = require("mason-tool-installer")
+
+    require("mason-null-ls").setup({
+      automatic_setup = true,
+      handlers = {},
+    })
 
     mason.setup({
       ui = {
